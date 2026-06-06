a96165dda2dcd6ceb5e232189263fb0f72494e52
Samuel Silva <samuel110407@gmail.com>
2026-06-05 21:43:09 -0300

    atualização do exemplo lista_encadeada_figuras

--- 05_lista_encadeada/04-lista_encadeada_figuras.c
+++ 05_lista_encadeada/04-lista_encadeada_figuras.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /*
     Este programa consiste em uma aplicação de lista encadeada, utilizando figuras geométricas.
@@ -70,6 +71,7 @@ void liberaLista(LISTA* lista) {
     LISTA* aux = lista;
     while(aux != NULL) {
         LISTA* p = aux->prox;
+        free(aux->info);
         free(aux);
         aux = p;
     }
@@ -77,20 +79,21 @@ void liberaLista(LISTA* lista) {
 
 void imprimeListaFiguras(LISTA* lista) {
     if (lista != NULL) {
+        printf("Lista:\n");
         LISTA* figura;
         int i = 1;
             printf("----------------------------");
             for (figura = lista; figura != NULL; figura = figura->prox) {
                 printf("\nFigura %d:\n - Tipo: %s\n", i, figura->tipo);
-                if (strcmp(figura->tipo, "RET")) {
+                if (strcmp(figura->tipo, "RET") == 0) {
                     RETANGULO* medidas = (RETANGULO*) figura->info;
                     printf(" - Base = %.2fm\n - Altura = %.2fm\n", medidas->base, medidas->altura);
                 }
-                else if (strcmp(figura->tipo, "TRI")) {
+                else if (strcmp(figura->tipo, "TRI") == 0) {
                     TRIANGULO* medidas = (TRIANGULO*) figura->info;
                     printf(" - Base = %.2fm\n - Altura = %.2fm\n", medidas->base, medidas->altura);
                 }
-                else if(strcmp(figura->tipo, "CIR")) {
+                else if(strcmp(figura->tipo, "CIR") == 0) {
                     CIRCULO* medidas = (CIRCULO*) figura->info;
                     printf(" - Raio = %.2fm\n", medidas->raio);
                 }
@@ -100,25 +103,33 @@ void imprimeListaFiguras(LISTA* lista) {
     }
 }
 
-LISTA* alocaFigura(char figuraChar, LISTA* listaFiguras) {
+LISTA* alocaFigura(char figura_char, LISTA* lista_figuras) {
     LISTA* figura = (LISTA*) malloc(sizeof(LISTA));
-    if (figuraChar == 'T' || figuraChar == 't') {
+    if (figura_char == 'T' || figura_char == 't') {
         strcpy(figura->tipo, "TRI");
         TRIANGULO* medidas = alocaTriangulo();
         figura->info = (void*) medidas; 
     }
-    else if (figuraChar == 'R' || figuraChar == 'r') {
+    else if (figura_char == 'R' || figura_char == 'r') {
         strcpy(figura->tipo, "RET");
         RETANGULO* medidas = alocaRetangulo();
         figura->info = (void*) medidas; 
     }
-    else if (figuraChar == 'C' || figuraChar == 'c') {
+    else if (figura_char == 'C' || figura_char == 'c') {
         strcpy(figura->tipo, "CIR");
         CIRCULO* medidas = alocaCirculo();
         figura->info = (void*) medidas; 
     }
-    figura->prox = listaFiguras;
+    figura->prox = lista_figuras;
+    
+    system("cls");
+    printf("\nFigura alocada com sucesso!");
+    sleep(2);
     return figura;
+}
+
+int verificaListaVazia(LISTA* lista) {
+    return lista == NULL;
 }
 
 LISTA* criaLista() {
@@ -126,22 +137,43 @@ LISTA* criaLista() {
 }
 
 int main (void) {
-    LISTA* listaFiguras = criaLista();
+    system("cls");
+    LISTA* lista_figuras = criaLista();
     int quantidade;
     printf("Digite a quantidade de figuras que serao inseridas na lista:\n");
-    scanf("%d", &quantidade);
-    char figura;
-    for (int i = 0; i < quantidade; i++) {
-        getchar();
-        printf("\nT = Triangulo\nR = Retangulo\nC = Circulo\nN = nenhum\n\nDigite o caractere da figura que deseja alocar: ");
-        scanf("%c", &figura);
-        if (figura == 'N' && figura == 'n') break;
-        else listaFiguras = alocaFigura(figura, listaFiguras);
+    
+    if(scanf("%d", &quantidade) != 1) {
+        printf("Entrada inválida.\n");
+        return 0;
     }
+    
+    if (quantidade > 0) {
+        char figura;
+        for (int i = 0; i < quantidade; i++) {
+            getchar();
+            system("cls");
+            printf("T = Triangulo\nR = Retangulo\nC = Circulo\nN = nenhum\n\nDigite o caractere da figura que deseja alocar: ");
+            
+            if(scanf("%c", &figura) != 1) {
+                printf("Entrada inválida.\n");
+                return 0;
+            }
+
+            if (figura == 'N' || figura == 'n') break;
+            else lista_figuras = alocaFigura(figura, lista_figuras);
+        }
+    }
+
+    system("cls");
     printf("\nFim da manutencao da lista!\n");
-    printf("Lista final:\n");
-    imprimeListaFiguras(listaFiguras);
-    liberaLista(listaFiguras);
+    if (verificaListaVazia(lista_figuras)) {
+        printf("A lista esta vazia.\n");
+    } else {
+        imprimeListaFiguras(lista_figuras);
+    }
+    sleep(3);
+
+    liberaLista(lista_figuras);
     printf("\nFim do programa.\n");
     exit(EXIT_SUCCESS);
 }
